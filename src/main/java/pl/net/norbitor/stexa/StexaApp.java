dc43e3b1c569f4c490009a3de898314ef9628ad6
Norbert Langner <norbitor@gmail.com>
2020-06-13 21:32:11 +0200

    Add Spring Boot

--- src/main/java/pl/net/norbitor/stexa/StexaApp.java
+++ src/main/java/pl/net/norbitor/stexa/StexaApp.java
@@ -1,7 +1,11 @@
 package pl.net.norbitor.stexa;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StexaApp {
     public static void main(String[] args) {
-        System.out.println("It works!");
+        SpringApplication.run(StexaApp.class, args);
     }
 }
